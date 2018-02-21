f8419ec213a9c08fcc5eafbceda1fd2046d83019
augustokal <augustokalel2@yahoo.com.br>
2018-02-21 17:20:35 -0300

    Revert "Testando"
    This reverts commit 38d48cb3c2b7869fe7b9c23bcd710e3521d57114.

--- src/subway/Subway.java
+++ src/subway/Subway.java
@@ -16,10 +16,13 @@ public class Subway {
      */
     public static void main(String[] args) {
         // testecfvcgxdgcxdAZD gfvrxzfdg
+<<<<<<< HEAD
         //testeeee
         //ola pra vc
         //mais uma alteracao
         aaaaaaaa
+=======
+>>>>>>> parent of 38d48cb... Testando
     }
     
 }
